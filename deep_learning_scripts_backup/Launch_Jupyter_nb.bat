73a2559b5ad809817b3505cdba09caad8186a422
vsbpan <vsbpan@gmail.com>
2023-12-12 15:13:32 -0700

    added deep learning backup files

---
+++ deep_learning_scripts_backup/Launch_Jupyter_nb.bat
@@ -0,0 +1,3 @@
+Call conda activate detectron_env
+cd C:\R_projects\deep_learning_playground
+Call jupyter lab
---
+++ deep_learning_scripts_backup/Launch_coco-annotator.bat
@@ -0,0 +1,3 @@
+cd C:\R_projects\deep_learning_playground\coco-annotator
+start "chrome.exe" http://localhost:5000/
+Call docker-compose up
