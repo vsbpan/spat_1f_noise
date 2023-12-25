5057ec6d9d24cf3e0f2737e2b04008d862e829a2
vsbpan <vsbpan@gmail.com>
2023-12-24 18:44:16 -0600

    updated image id fetch method

--- data_dict_to_COCO.R
+++ data_dict_to_COCO.R
@@ -13,3 +13,32 @@ export_COCO(as.Json(fetch_data_dict(50)) %>%
 
 
 obj$images$events[[1]] %>% length()
+
+
+
+db <- import_COCO("annotations/COCO_database.json")
+
+db <- db$images %>% filter(dataset_id == "2")
+
+dbdf <- data.frame(
+  "id" = db$id,
+  "file_name" = db$file_name
+)
+
+
+
+a$images
+
+a <- as.Json(fetch_data_dict(50), db = dbdf)
+a <- a %>% set_new_path(path_root = "datasets/rep50")
+
+
+
+
+
+
+
+
+
+
+
