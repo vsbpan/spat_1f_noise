7642d243b6bfb0ebdeb6e8b3d9252b90e9894848
vsbpan <vsbpan@gmail.com>
2024-01-11 15:21:15 -0700

    added events

--- inference_processing.R
+++ inference_processing.R
@@ -24,18 +24,17 @@ list.files("cleaned_data/data_dicts/")
 #   }, cores = 8, inorder = FALSE, export_fun_only = TRUE
 # )
 
-pb_par_lapply(list.files("cleaned_data/data_dicts/", full.names = TRUE),
-    function(x){
-      
-    save_path <- paste0("cleaned_data/events/", gsub(".rds",".csv",basename(x)))
-    if(file.exists(save_path)){
-        return(NULL)
-    } else {
-      l <- readRDS(x)
-      d <- get_data(l)
-      write_csv(d, save_path) 
-    }
-  }, cores = 8, inorder = FALSE, export_fun_only = TRUE)
-
+# pb_par_lapply(list.files("cleaned_data/data_dicts/", full.names = TRUE),
+#     function(x){
+#       
+#     save_path <- paste0("cleaned_data/events/", gsub(".rds",".csv",basename(x)))
+#     if(file.exists(save_path)){
+#         return(NULL)
+#     } else {
+#       l <- readRDS(x)
+#       d <- get_data(l)
+#       write_csv(d, save_path) 
+#     }
+#   }, cores = 8, inorder = FALSE, export_fun_only = TRUE)
 
 
