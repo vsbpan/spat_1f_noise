f1e9e6e691434fce2aa03ea8ffc043783cf8908f
vsbpan <vsbpan@gmail.com>
2024-01-16 08:22:03 -0700

    changed relative path to abs path for init.R

--- helper_functions/init.R
+++ helper_functions/init.R
@@ -3,4 +3,4 @@ library(doSNOW)
 #library(aster)
 
 # Some custom functions in "./helper_functions/R/*.R" are loaded as a simulated 'spat1f' package
-devtools::load_all(path = "helper_functions", export_all = TRUE, quiet = TRUE)
+devtools::load_all(path = "C:/R_projects/spat_1f_noise/helper_functions", export_all = TRUE, quiet = TRUE)
