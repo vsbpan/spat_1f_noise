62cf9b0b2387997ad092ac697b5655a3a910f5e0
vsbpan <vsbpan@gmail.com>
2024-05-03 08:56:59 -0400

    updated COCOutils to handle images with no annotations

--- data_processing/data_dict_to_COCO.R
+++ data_processing/data_dict_to_COCO.R
@@ -1,13 +1,14 @@
 source("spat1f/init.R")
 
-
+#### Export mask-R-CNN predictions for a set of images in COCO Json format ####
+# Read file names of sampled images
 s <- list.files(
   "C:/R_projects/deep_learning_playground/coco-annotator/datasets/sample2", 
   recursive = FALSE, full.names = FALSE
 )
 s
 
-
+# Find the model inference from stored data_dicts. 
 coco_list <- lapply(
   fetch_repID(), 
   function(x){
@@ -21,33 +22,49 @@ coco_list <- do.call("merge_COCO",coco_list)
 
 coco_list <- coco_list %>% set_new_path("/datasets/sample2/")
 
-export_COCO(coco_list, "annotations/sample2_modelv6_inference.json")
+# Export annotations
+# export_COCO(coco_list, "annotations/sample2_modelv6_inference.json")
 
-# 
-# 
-# a <- import_COCO("annotations/sample1_manual_annotation.json")
-# a$images <- a$images %>% 
-#   dplyr::select(all_of(names(coco_list$images))) %>% 
-#   as.data.frame()
-# a$categories <- a$categories %>% 
-#   dplyr::select(all_of(names(coco_list$categories))) %>% 
-#   as.data.frame()
-# a$annotations <- a$annotations %>% 
-#   dplyr::select(all_of(names(coco_list$annotations))) %>% 
-#   as.data.frame()
-# 
-# 
-# new_files <- s[!s %in% a$images$file_name]
-# 
-# 
-# coco_final <- merge_COCO(subset_COCO(coco_list, new_files), a)
-# 
-# coco_final$categories <- coco_final$categories[1,]
-# coco_final <- coco_final %>% set_new_path("/datasets/sample2/")
-# 
-# coco_final
-# 
-# export_COCO(coco_final, "annotations/modelv6_inference_with_manual.json")
+
+
+
+#### Clean and merge annotations exported from COCO Annotator ####
+
+# Take the set of all images in a sample, wipe the annotations, and append any that is found in manual_COCO. Repeat for sample 1 and sample 2. 
+# I annotated the images in three rounds. a set of ~400 images were used to train modelv5, which I used to do semi-automated/manual annotations for sample 1. Sample 1 was used to train modelv6, which I used for semi-automated/manual annotations for sample 2. The final dataset is what is used to train the final model, i.e. modelv7
+a1 <- import_COCO("annotations/sample1_modelv5_inference.json")
+b1 <- import_COCO("annotations/sample1_manual_annotation_updated.json")
+
+z <- update_manual_COCO(a1, b1)
+z$categories <- z$categories[1,]
+
+
+a <- import_COCO("annotations/sample2_modelv6_inference.json")
+b <- import_COCO("annotations/sample2_manual_annotation.json")
+
+z2 <- update_manual_COCO(a, b)
+z2$categories <- z2$categories[1,]
+
+# Merge the two samples together
+final_COCO <- merge_COCO(z, z2)
+
+# Write the root path for where to look for the image files
+final_COCO <- final_COCO %>% set_new_path("/datasets/master_sample/")
+
+# Export master annotation
+# export_COCO(final_COCO, path = "annotations/master_sample_manual_annotation.json")
+
+
+#### Data splitting ####
+
+final_COCO <- import_COCO("annotations/master_sample_manual_annotation.json")
+
+final_COCO_split <- split_COCO(final_COCO, test = 0.1, val = 0.05)
+
+
+# export_COCO(final_COCO_split$train, "annotations/coco_train.json")
+# export_COCO(final_COCO_split$test, "annotations/coco_test.json")
+# export_COCO(final_COCO_split$val, "annotations/coco_val.json")
 
 
 
--- prediction_evaluator.R
+++ prediction_evaluator.R
@@ -2,10 +2,10 @@ source("spat1f/init.R")
 
 
 
-d <- read_csv("raw_data/sample1_modelv6_validation_inference.csv") %>% 
+d <- read_csv("raw_data/sample1_modelv7_validation_inference.csv") %>% 
   mutate(
-    polygon = ifelse(type == "prediction" & score < 0.7, NA, polygon),
-    keypoints = ifelse(type == "prediction" & score < 0.7, NA, keypoints)
+    polygon = ifelse(type == "prediction" & score < 0.9, NA, polygon),
+    keypoints = ifelse(type == "prediction" & score < 0.9, NA, keypoints)
   )
 pred <- spat1f::parse_inference(d %>% 
                               filter(type == "prediction"), 
@@ -20,35 +20,6 @@ keypoint_evaluator(pred, gt, k = c(100), keypoints = c("head"))
 mask_evaluator(pred, gt)
 
 detection_report(fetch_repID())
-
-
-
-
-
-
-fetch_data_dict(5)
-
-
-
-gt <- as.data_dict(import_COCO("annotations/sample1_manual_annotation.json"))
-pred <- fetch_repID() %>% 
-  lapply(
-    function(k){
-      fetch_data_dict(k)
-    }
-  ) %>% 
-  do.call("c", .)
-
-pred <- pred[which(get_file_meta(pred)$file_base %in% get_file_meta(gt)$file_base)]
-
-gt <- gt[which(get_file_meta(gt)$file_base %in% get_file_meta(pred)$file_base)]
-
-pred <- pred[match(get_file_meta(gt)$file_base, get_file_meta(pred)$file_base)]
-
-keypoint_evaluator(pred, gt, k = c(100), keypoints = c("head"))
-mask_evaluator(pred, gt)
-
-
 
 
 
